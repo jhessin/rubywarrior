e3d772ebfcede9514260c9410d15a1caa2110048
Jim Hessin <jhessin@gmail.com>
2020-03-12 18:12:49 -0500

    vifm Generated Update

--- grillbrick-beginner/player.rb
+++ grillbrick-beginner/player.rb
@@ -1,11 +1,32 @@
 class Player
   def play_turn(warrior)
-    if !warrior.feel.empty?
-      warrior.attack!
-    elsif warrior.health <= 18
-      warrior.rest!
+    @health ||= warrior.health
+
+    if warrior.health < @health
+      # taking damage
+      # update health
+      @health = warrior.health
+
+      if warrior.feel.captive?
+        warrior.rescue!
+      elsif warrior.feel.empty?
+        warrior.walk!
+      else
+        warrior.attack!
+      end
+    elsif warrior.feel.empty?
+      # safe, heal if necessary
+      if warrior.health <= 18
+        warrior.rest!
+        @health = warrior.health
+      else
+        warrior.walk!
+      end
+    elsif warrior.feel.captive?
+      warrior.rescue!
     else
-      warrior.walk!
+      # enemy! attack!
+      warrior.attack!
     end
   end
 end
